95b86b0dc5ec315323deff22c8711b4b276294f8
Chris <krzy.driwa@gmail.com>
2023-09-20 20:18:15 +0200

    Create square div grid constructor

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+const GRID_SIZE = 16;
+
+const gridContainer = document.querySelector('.grid-container');
+
+
+function createSquare() {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    return square;
+}
+
+function createRow(gridSize) {
+    let row = document.createElement('row');
+    row.classList.add('row');
+    for (let i = 0; i < gridSize; i++) {
+        row.appendChild(createSquare());
+    }
+    return row;
+}
+
+function gridConstructor(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        gridContainer.appendChild(createRow(gridSize));
+    }
+}
+
+gridConstructor(GRID_SIZE);
